b9b9d5dd2caae802a4511c0193629681735e6dae
Dario-B0 <dariobasile55@gmail.com>
2022-01-28 21:48:34 +0100

    al file vengono aggiunte solo le pose dopo uno spostamento significativo

--- workspace/src/slam/src/graph_node.cpp
+++ workspace/src/slam/src/graph_node.cpp
@@ -10,7 +10,13 @@
 #include "nav_msgs/Odometry.h"
 
 int T = 100;
+
 int next_position = 0;
+bool conect_pose = false;
+
+std::vector<float> old_position(3, 0);
+std::vector<float> current_position(3, 0);
+
 std::ofstream oFile("graph.txt", std::ios_base::out | std::ios_base::trunc);
 
 tf2_ros::Buffer tfBuffer;
@@ -18,15 +24,32 @@ tf2_ros::Buffer tfBuffer;
 void odometryCallback(const nav_msgs::OdometryConstPtr &odom)
 {
 
-    if (oFile.is_open()){
-            oFile << "VERTEX_SE2 " 
-            << "<" << next_position << "> "
-            << "<" << odom->pose.pose.position.x << "> "
-            << "<" << odom->pose.pose.position.y << "> "
-            << "<" << odom->pose.pose.orientation.w << "> "
-            << "\n";
-    next_position++;
+    current_position[0] = odom->pose.pose.position.x;
+    current_position[1] = odom->pose.pose.position.y;
+    current_position[2] = odom->pose.pose.orientation.w;
 
+    float distance = sqrt(pow(current_position[0] - old_position[0], 2) + pow(current_position[1] - old_position[1], 2));
+ 
+    if (distance > 0.1 || current_position[2] - old_position[2] > 0.5)
+    {
+        old_position[0] = current_position[0];
+        old_position[1] = current_position[1];
+        old_position[2] = current_position[2];
+
+        if (oFile.is_open())
+        {
+            oFile << "VERTEX_SE2 "
+                      << "<" << next_position << "> "
+                      << "<" << odom->pose.pose.position.x << "> "
+                      << "<" << odom->pose.pose.position.y << "> "
+                      << "<" << odom->pose.pose.orientation.w << "> "
+                      << "\n";
+            next_position++;
+            /*if (conect_pose){
+               
+            }*/
+            conect_pose = true;
+        }
     }
     return;
 }
