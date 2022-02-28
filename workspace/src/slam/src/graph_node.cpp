6a78e06f1bdfb9473314033115a303057d4f2904
Dario-B0 <dariobasile55@gmail.com>
2022-02-28 18:19:39 +0100

    piccole correzioni e aggiunta commenti

--- workspace/src/slam/src/graph_node.cpp
+++ workspace/src/slam/src/graph_node.cpp
@@ -1,26 +1,19 @@
 #include <fstream>
 #include <iostream>
-#include <string>
 #include <ros/ros.h>
-#include "vector"
-#include "set"
-#include "tf/tf.h"
-#include "tf2_msgs/TFMessage.h"
-#include "tf2/convert.h"
-#include "tf2_ros/transform_listener.h"
-#include "geometry_msgs/TransformStamped.h"
-#include "geometry_msgs/PoseWithCovarianceStamped.h"
-#include "nav_msgs/Odometry.h"
 #include <tf2/LinearMath/Quaternion.h>
 #include <tf2/LinearMath/Matrix3x3.h>
 #include <tf2/LinearMath/Transform.h>
 #include <tf2/LinearMath/Vector3.h>
 #include <apriltag_ros/AprilTagDetectionArray.h>
+#include "vector"
+#include "set"
+#include "tf2_ros/transform_listener.h"
+#include "geometry_msgs/TransformStamped.h"
+#include "nav_msgs/Odometry.h"
 #include "sensor_msgs/LaserScan.h"
 
 using namespace std;
-
-int T = 100;
 
 int next_position = 1000;
 int next_view_position = 1000;
@@ -33,15 +26,14 @@ sensor_msgs::LaserScan last_scan;
 
 //ultima posizione del robot aggiunta al grafo
 vector<float> last_pose = {0, 0, 0, 0, 0, 0};
-ros::Time last_time;
 
 //posizione attuale del robot
 vector<float> current_pose = {0, 0, 0, 0, 0, 0};
-ros::Time current_time;
 
-map<int, set<int>> tag_in;
+//set di tag individuati
+set<int> tag_in;
 
-ofstream oFile("graph.g2o", ios_base::out | ios_base::trunc);
+ofstream oFile("graph_rect.txt", ios_base::out | ios_base::trunc);
 
 tf2_ros::Buffer tfBuffer;
 
@@ -62,15 +54,15 @@ void odometryCallback(const nav_msgs::OdometryConstPtr &odom)
     current_pose[4] = pitch;
     current_pose[5] = yaw;
 
-    current_time = odom->header.stamp;
-
+    //posa corrente espressa nel frame di quella precedente
     float dx = current_pose[0] - last_pose[0];
     float dy = current_pose[1] - last_pose[1];
     float dth = current_pose[5] - last_pose[5];
 
     float distance = sqrt(pow(dx, 2) + pow(dy, 2));
 
-    if (distance > 0.1 || dth > 0.5 || !conect_pose)
+    //salva solo uno spostamento (traslazione o rotazione) rilevante
+    if (distance > 0.1 || dth < -0.5 || dth > 0.5 || !conect_pose)
     {
         last_pose[0] = current_pose[0];
         last_pose[1] = current_pose[1];
@@ -79,32 +71,30 @@ void odometryCallback(const nav_msgs::OdometryConstPtr &odom)
         last_pose[4] = current_pose[4];
         last_pose[5] = current_pose[5];
 
-        last_time = current_time;
+        //nodo VERTEX_SE2 <id> <x> <y> <theta> al grafo
+        oFile << "VERTEX_SE2"
+              << " " << next_position
+              << " " << current_pose[0]
+              << " " << current_pose[1]
+              << " " << current_pose[5]
+              << "\n";
 
-        if (oFile.is_open())
+        if (conect_pose)
         {
-            oFile << "VERTEX_SE2"
+            //arco EDGE_SE2 <id-1> <id> <dx> <dy> <dth> al grafo
+            oFile << "EDGE_SE2"
+                  << " " << next_position - 1
                   << " " << next_position
-                  << " " << current_pose[0]
-                  << " " << current_pose[1]
-                  << " " << current_pose[5]
+                  << " " << dx
+                  << " " << dy
+                  << " " << dth
+                  << " 500 0 0 500 0 5000"
                   << "\n";
-
-            if (conect_pose)
-            {
-                oFile << "EDGE_SE2"
-                      << " " << next_position - 1
-                      << " " << next_position
-                      << " " << dx
-                      << " " << dy
-                      << " " << dth
-                      << " 500 0 0 500 0 5000"
-                      << "\n";
-            }
-            next_position++;
-            conect_pose = true;
         }
+        next_position++;
+        conect_pose = true;
     }
+
     return;
 }
 
@@ -113,6 +103,8 @@ void tagDetectionsCallback(const apriltag_ros::AprilTagDetectionArrayConstPtr &d
     stop = false;
     if (detection_msg->detections.size() == 0)
         return;
+
+    //al primo tag individuato calcola la trasformazione tra la pose della camera e del robot
     if (first_detect)
     {
         geometry_msgs::TransformStamped transformStamped;
@@ -134,10 +126,11 @@ void tagDetectionsCallback(const apriltag_ros::AprilTagDetectionArrayConstPtr &d
         first_detect = false;
     }
 
+    //trasformazione tra la poszione corrente del robot e l'ultima aggiunta al grafo
     tf2::Quaternion q;
     q.setRPY(current_pose[3] - last_pose[3], current_pose[4] - last_pose[4], current_pose[5] - last_pose[5]);
     tf2::Vector3 v(current_pose[0] - last_pose[0], current_pose[1] - last_pose[1], current_pose[2] - last_pose[2]);
-    tf2::Transform T(q, v); //trasformazione tra la poszione corrente del robot e l'ultima aggiunta al grafo
+    tf2::Transform T(q, v);
 
     int tag_id;
 
@@ -146,57 +139,46 @@ void tagDetectionsCallback(const apriltag_ros::AprilTagDetectionArrayConstPtr &d
         tag_id = tag.id[0];
 
         //trasformazione dal frame della camera al frame dell'odometria
+        tf2::Vector3 tag_position_cf(tag.pose.pose.pose.position.x, tag.pose.pose.pose.position.y, tag.pose.pose.pose.position.z);
+        tf2::Vector3 tag_position_rf_now = robot_camera_T * tag_position_cf;
 
-        bool new_tag = tag_in.find(tag_id) == tag_in.end();
+        //verifica che nel punto in cui ho individuato il tag ci sia effettivamente
+        //una parete, in modo da poter scartare tag erroneamente rilevati
+        float theta_tag = atan2(tag_position_rf_now.y(), tag_position_rf_now.x());
+        float theta_range = last_scan.angle_min;
+        std::vector<float> ranges = last_scan.ranges;
 
-        if (new_tag || tag_in[tag_id].find(next_position) == tag_in[tag_id].end())
+        int k = 0;
+        while (k < ranges.size())
         {
-
-            tf2::Vector3 tag_position_cf(tag.pose.pose.pose.position.x, tag.pose.pose.pose.position.y, tag.pose.pose.pose.position.z);
-            tf2::Vector3 tag_position_rf_now = robot_camera_T * tag_position_cf;
-
-            float theta_tag = atan2(tag_position_rf_now.y(), tag_position_rf_now.x());
-            float theta_range = last_scan.angle_min;
-            std::vector<float> ranges = last_scan.ranges;
-
-            int k = 0;
-            while (k < ranges.size())
+            if (theta_range > theta_tag)
             {
-                if (theta_range > theta_tag)
-                {
-                    break;
-                }
-                theta_range += last_scan.angle_increment;
-                k++;
+                break;
             }
-            if(k==ranges.size() || theta_range-theta_tag>theta_tag-(theta_range-last_scan.angle_increment)) k--;
-            float scan_distance = ranges[k];
-            float tag_distance = sqrt(pow(tag_position_rf_now.x(), 2) + pow(tag_position_rf_now.y(), 2));
-            float error = scan_distance - tag_distance;
+            theta_range += last_scan.angle_increment;
+            k++;
+        }
+        if (k == ranges.size() || theta_range - theta_tag > theta_tag - (theta_range - last_scan.angle_increment))
+            k--;
+        float scan_distance = ranges[k];
+        float tag_distance = sqrt(pow(tag_position_rf_now.x(), 2) + pow(tag_position_rf_now.y(), 2));
+        float error = scan_distance - tag_distance;
 
-            tf2::Vector3 tag_position_rf = T * tag_position_rf_now;
+        if (error < -0.5 || error > 0.5)
+            continue;
 
-            if (error < -0.5 || error > 0.5) continue;
+        tf2::Vector3 tag_position_rf = T * tag_position_rf_now;
+        if (sqrt(pow(tag_position_rf.x(), 2) + pow(tag_position_rf.y(), 2) + pow(tag_position_rf.z(), 2)) > 5)
+            continue;
 
-            /*if (error < -0.5 || error > 0.5 || tag_position_rf.z()<-0.2 || tag_position_rf.z()>0.5 ||tag_id>9)
-            {
-                cerr <<endl<< tag_id <<" : discrepanza tra laser scan e tag detection: " << error ;
-                //continue;
-            }
-
-            if(tag_position_rf.z()<-0.2 || tag_position_rf.z()>0.5 || tag_id>9) {
-                cerr << "  TAG ERRATO";
-                //continue;
-                }*/
-
-            if (new_tag)
-            {
+        //tag incontrato per la prima volta
+        if (tag_in.find(tag_id) == tag_in.end())
+        {
             geometry_msgs::TransformStamped transformStamped;
-            //geometry_msgs::TransformStamped transformStamped2;
             try
             {
-                transformStamped = tfBuffer.lookupTransform("odom", "fisheye_rect", current_time, ros::Duration(3));
-                //transformStamped2 = tfBuffer.lookupTransform("odom", "laser_frame", last_scan.header.stamp, ros::Duration(3));
+                //trasformazione dal frame della camera al frame del mondo (odometria)
+                transformStamped = tfBuffer.lookupTransform("odom", "fisheye_rect", ros::Time(0), ros::Duration(3));
             }
             catch (tf2::TransformException &ex)
             {
@@ -205,58 +187,42 @@ void tagDetectionsCallback(const apriltag_ros::AprilTagDetectionArrayConstPtr &d
                 return;
             }
 
-            set<int> map_set{};
-            tag_in[tag_id] = map_set;
+            tag_in.insert(tag_id);
 
-            //trasformazione dal frame della camera al frame dell'odometria
             tf2::Quaternion q2;
             q2 = tf2::Quaternion(transformStamped.transform.rotation.x, transformStamped.transform.rotation.y, transformStamped.transform.rotation.z, transformStamped.transform.rotation.w);
             tf2::Vector3 v2 = tf2::Vector3(transformStamped.transform.translation.x, transformStamped.transform.translation.y, transformStamped.transform.translation.z);
             tf2::Transform T2 = tf2::Transform(q2, v2);
             tf2::Vector3 tag_position_wf = T2 * tag_position_cf;
 
-            /////
-            /*tf2::Quaternion q3;
-            q3= tf2::Quaternion(transformStamped2.transform.rotation.x, transformStamped2.transform.rotation.y, transformStamped2.transform.rotation.z, transformStamped2.transform.rotation.w);
-            tf2::Vector3 v3 = tf2::Vector3(transformStamped2.transform.translation.x, transformStamped2.transform.translation.y, transformStamped2.transform.translation.z);
-            tf2::Transform T3 = tf2::Transform(q3, v3);*/
-
-            ////
-
-            //cerr << "\ntag_id: "<<tag_id<<"\nworld " << tag_position_wf.x()  << " " << tag_position_wf.y() <<" " << tag_position_wf.z();
-            //cerr << "\nworld2 " << tag_position_wf_alt.x() << " " << tag_position_wf_alt.y()<< " " << tag_position_wf_alt.z();
-
-            //aggiungi nodo VERTEX_XY <xw> <yw> al grafo
+            //nodo VERTEX_XY <tag_id> <xw> <yw> al grafo
             oFile << "VERTEX_XY"
-                      << " " << tag_id
-                      << " " << tag_position_wf.x() << " " << tag_position_wf.y() << "\n";
-            }
-
-            //aggiungi arco EDGE_SE2_XY <id_pose> <id_tag> <xr> <yr> al grafo
-            oFile << "EDGE_SE2_XY"
-                  << " " << next_position - 1 << " " << tag_id
-                  << " " << tag_position_rf.x() << " " << tag_position_rf.y() << " 1000 0 1000"
-                  << "\n";
-            //float distance=sqrt(pow(tag_position_rf.x(),2)+pow(tag_position_rf.y(),2)+pow(tag_position_rf.z(),2));
-            tag_in[tag_id].insert(next_position);
-            //cerr << "\ncamera " << tag_position_cf.x() << " " << tag_position_cf.y() << " " << tag_position_cf.z()
-            //cerr << "\nrobot "<< tag_position_rf.x() << " " << tag_position_rf.y()  <<" "<< tag_position_rf.z() <<"  "<<next_position <<" distance: "<<distance<< endl;
+                  << " " << tag_id
+                  << " " << tag_position_wf.x() << " " << tag_position_wf.y() << "\n";
         }
+
+        //arco EDGE_SE2_XY <id_pose> <id_tag> <xr> <yr> al grafo
+        oFile << "EDGE_SE2_XY"
+              << " " << next_position - 1 << " " << tag_id
+              << " " << tag_position_rf.x() << " " << tag_position_rf.y() << " 1000 0 1000"
+              << "\n";
     }
     return;
 }
 
+//salva in una variabile globale lo scan più recente
 void scanCallback(const sensor_msgs::LaserScan::ConstPtr &msg_)
 {
     last_scan = *msg_;
 }
 
+//termina l'esecuzione quando non riceve più messaggi su nessun topic
 void check_Callback(const ros::TimerEvent &event)
 {
     if (started && stop)
     {
         oFile.close();
-        cerr << "\nENDING PROCEDURE" << endl;
+        cerr << "\nNODE SHUTDOWN" << endl;
         exit(0);
     }
     stop = true;
@@ -266,7 +232,7 @@ int main(int argc, char **argv)
 {
     ros::init(argc, argv, "graph_node");
     ros::NodeHandle nh;
-    ros::Subscriber sub_tf = nh.subscribe("/odom", 1000, odometryCallback);
+    ros::Subscriber sub_odom = nh.subscribe("/odom", 1000, odometryCallback);
     ros::Subscriber sub_tag_detc = nh.subscribe("/tag_detections", 1000, tagDetectionsCallback);
     ros::Subscriber sub_scan = nh.subscribe("/base_scan", 1000, scanCallback);
 
@@ -274,6 +240,4 @@ int main(int argc, char **argv)
     ros::Timer timer1 = nh.createTimer(ros::Duration(5), check_Callback);
 
     ros::spin();
-
-    return 0;
 }
